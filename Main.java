225e5b46ee4d58960a1dbc9aa937f02d25fdcdd1
Pascal Hoefler <pascal.hoefler@gmx.at>
2011-05-03 23:50:08 +0200

    <Tag 2> Pascal: 2.Aenderung gemacht.

--- Main.java
+++ Main.java
@@ -22,6 +22,7 @@ public class Main {
 		objectSet.add(new DomainObject("Claudia"));
 		objectSet.add(new DomainObject("Pascal"));
 		objectSet.add(new DomainObject("Mario"));
+		objectSet.add(new DomainObject("Gitti"));
 
 		for (DomainObject domainObject : objectSet) {
 			LOGGER.debug(domainObject.id);
